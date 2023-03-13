633c5d449bbeb4c8aa46cb5563008d46a92d6c18
bdikbas <122579715+bdikbas@users.noreply.github.com>
2023-03-12 18:06:30 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -4,9 +4,9 @@ rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
 
+cd student-submission
 
-
-if [[ -e student-submission/ListExamples.java ]]
+if [[ -e ListExamples.java ]]
 then
   echo 'ListExamples.java found'
 else
